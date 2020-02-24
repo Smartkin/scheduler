9ca4b65c9498fc23fdccdc303431931c054b9da2
a-sey <a_sey@bk.ru>
2020-02-24 18:33:16 +0300

    BackEnd: По любимой привычке забыл подвязать к функционалу API, поправил

--- backend/src/main/java/com/dvoeizlarza/scheduler/controller/api/LessonDateController.java
+++ backend/src/main/java/com/dvoeizlarza/scheduler/controller/api/LessonDateController.java
@@ -1,5 +1,7 @@
 package com.dvoeizlarza.scheduler.controller.api;
 
+import com.dvoeizlarza.scheduler.dto.DisciplineDto;
+import com.dvoeizlarza.scheduler.dto.NoteDto;
 import com.dvoeizlarza.scheduler.entity.LessonDate;
 import com.dvoeizlarza.scheduler.service.LessonDateService;
 import com.dvoeizlarza.scheduler.view.LessonDateView;
@@ -7,10 +9,7 @@ import com.dvoeizlarza.scheduler.viewconverter.LessonDateViewConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -24,18 +23,25 @@ public class LessonDateController {
 
     @GetMapping
     @ResponseBody
-    LessonDateView read(Long id){
+    LessonDateView read(Long id) {
         return lessonDateViewConverter.convert(lessonDateService.read(id));
     }
 
     @GetMapping("list")
     @ResponseBody
-    List<LessonDateView> readList(@RequestParam Long schId, @RequestParam String date, @RequestParam int count){
+    List<LessonDateView> readList(@RequestParam Long schId, @RequestParam String date, @RequestParam int count) {
         List<LessonDate> lessonDates = lessonDateService.readList(schId, LocalDate.parse(date), count);
         return lessonDates.stream()
                 .map(lessonDateViewConverter::convert)
                 .sorted(LessonDateView.COMPARE_BY_START)
                 .collect(Collectors.toList());
+    }
+
+    @PostMapping("modify")
+    @ResponseBody
+    ResponseEntity modify(@RequestParam Long id, @RequestBody NoteDto dto){
+        lessonDateService.modify(dto);
+        return ResponseEntity.ok("Success");
     }
 
     @Autowired
